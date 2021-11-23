11c8a9c9fd76052e22326507c7edab7656591e9a
mitti3 <89256593+kitsun342@users.noreply.github.com>
2021-11-23 13:57:54 +0500

    js-test

---
+++ script.js
@@ -0,0 +1,25 @@
+var yourName = prompt('Введите ваше имя: ' )
+var yourAge = prompt('Ваш возраст: ' )
+var data = alert('Ваше имя ' + yourName + ' Вам ' + yourAge + ' лет.' + ' Что ж приступим к примерам)!')
+
+var plus = prompt('1 + 1 = ' )
+var minus = prompt('20 - 1 = ' )
+var multiplication = prompt('5 * 60 = ' )
+var devesion = prompt('125 / 5 = ' )
+var remainderDevesion = prompt('5 / 2 = ' )
+
+console.log('Ваше имя ' + yourName + ' Вам ' + yourAge + ' лет.');
+console.log('1 + 1 = 2 ' + 'Ваш ответ: ' + plus);
+console.log('20 - 1 = 19 ' + 'Ваш ответ: ' + minus);
+console.log('5 * 60 = 300 ' + 'Ваш ответ: ' + multiplication);
+console.log('125 / 5 = 25 ' + 'Ваш ответ: ' + devesion);
+console.log('5 / 2 = 1 ' + 'Ваш ответ: ' + remainderDevesion);
+
+var theEnd = alert('Конец! Проверьте результаты в консоли разрабочика.')
+
+
+
+
+
+
+
